759e2373e2188a6f4051aeedbed7f4df6e4fdedd
吴鑫博 <wuxbl@digitalchina.com>
2021-01-28 09:39:12 +0800

    移除废弃引入

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { default as PageContainer } from "./PageContainer";
 export { default as RadialBar } from "./RadialBar";
 export { default as HorizontalBar } from "./HorizontalBar";
 export { default as Area } from "./Area";
-export { LangConfig, LangContext } from "./LocaleConfig";
