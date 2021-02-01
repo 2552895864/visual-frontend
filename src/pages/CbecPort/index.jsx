fb9fc7dfc8a753a3cf7423f0b9e1903d6cafd018
肖子立 <xiaozlb@digitalchina.com>
2021-02-01 14:48:44 +0800

    SquareGrid

--- src/pages/CbecPort/index.jsx
+++ src/pages/CbecPort/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import intl from "react-intl-universal";
-import { PageContainer } from "@/components";
+import { PageContainer, SquareGrid } from "@/components";
 // import styles from "./index.module.less";
 
 const CebcPort = () => {
   return (
     <PageContainer>
-      <div>{"CebcPort"}</div>
+      <div>
+        <SquareGrid></SquareGrid>
+      </div>
     </PageContainer>
   );
 };
