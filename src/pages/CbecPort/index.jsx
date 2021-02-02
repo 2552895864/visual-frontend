fae322d05881f2f32a66a48d09dda75f62d396c9
肖子立 <xiaozlb@digitalchina.com>
2021-02-02 16:01:15 +0800

    LineChart

--- src/pages/CbecPort/index.jsx
+++ src/pages/CbecPort/index.jsx
@@ -3,14 +3,14 @@ import React from "react";
 // import { PageContainer, SquareGrid, Table } from "@/components";
 import { PageContainer } from "@/components";
 // import { CumulativeValue, ProgressBar } from "./modules";
-import { EchartPie } from "./modules";
+import { LineChart } from "./modules";
 import styles from "./index.module.less";
 
 const CebcPort = () => {
   return (
     <PageContainer>
       <div>
-        <EchartPie className={styles.pie}></EchartPie>
+        <LineChart className={styles.line}></LineChart>
       </div>
     </PageContainer>
   );
--- src/pages/CbecPort/modules/index.js
+++ src/pages/CbecPort/modules/index.js
@@ -2,3 +2,4 @@ export { default as CumulativeValue } from "./CumulativeValue";
 export { default as FlipperCard } from "./FlipperCard";
 export { default as ProgressBar } from "./ProgressBar";
 export { default as EchartPie } from "./EchartPie";
+export { default as LineChart } from "./LineChart";
