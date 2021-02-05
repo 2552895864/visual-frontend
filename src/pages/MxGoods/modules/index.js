af2e5c5302568602a38305a8aa38b4526a68bdee
肖子立 <xiaozlb@digitalchina.com>
2021-02-05 17:44:15 +0800

    Popup

--- src/pages/MxGoods/modules/index.js
+++ src/pages/MxGoods/modules/index.js
@@ -3,3 +3,4 @@ export { default as Scan } from "./Scan";
 export { default as Overview } from "./Overview";
 export { default as BreathingLights } from "./BreathingLights";
 export { default as Statistic } from "./Statistic";
+export { default as Popup } from "./Popup";
