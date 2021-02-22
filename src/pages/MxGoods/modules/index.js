2935505bec4583c71e1619103f713b0254c69a9f
肖子立 <xiaozlb@digitalchina.com>
2021-02-22 18:19:20 +0800

    弹窗切换

--- src/pages/MxGoods/modules/index.js
+++ src/pages/MxGoods/modules/index.js
@@ -4,3 +4,4 @@ export { default as Overview } from "./Overview";
 export { default as BreathingLights } from "./BreathingLights";
 export { default as Statistic } from "./Statistic";
 export { default as Popup } from "./Popup";
+export { default as AnnouncementTable } from "./AnnouncementTable";
