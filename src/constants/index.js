d5560c701cf000a8c5143a04729205d90b2c07c4
肖子立 <xiaozlb@digitalchina.com>
2021-02-22 11:12:35 +0800

    change name

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const DEFAULT_TITLE = "智慧口岸效能检测大屏";
+export const DEFAULT_TITLE = "湖北数字商务平台";
 export const MENU_TRIGGER_NAME = "大屏菜单";
 export const MENU_TRIGGER_EN_NAME = "The menu";
 export const MENU_TITLE = "智慧大屏菜单";
