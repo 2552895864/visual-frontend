0e0bac084cb47ebd4eb8625cb2f2eacb5933963a
吴鑫博 <wuxbl@digitalchina.com>
2021-02-25 15:25:05 +0800

    更新跨境电商界面

--- src/pages/Cbec/Trend.jsx
+++ src/pages/Cbec/Trend.jsx
@@ -1,57 +1,57 @@
 import React, { useEffect } from "react";
-// import intl from "react-intl-universal";
 import echarts from "echarts";
 import { ModuleContainer } from "@/components";
 import tools from "@/utils/echarts-tooltip-auto-show";
 import getChartsOption from "./options/rank";
 import styles from "./Trend.module.less";
 
-const Trend = () => {
-  const options = getChartsOption({
-    xCoordinate: [
-      "1月",
-      "2月",
-      "3月",
-      "4月",
-      "5月",
-      "6月",
-      "7月",
-      "8月",
-      "9月",
-      "10月",
-      "11月",
-      "12月",
-    ],
-    barData: [
-      33000,
-      31000,
-      36000,
-      32500,
-      34500,
-      31500,
-      33000,
-      31000,
-      36000,
-      32500,
-      34500,
-      31500,
-    ],
-    lineData: [
-      32000,
-      29000,
-      34500,
-      25800,
-      34000,
-      33000,
-      22000,
-      29000,
-      24500,
-      35800,
-      24000,
-      35000,
-    ],
-  });
+const data = {
+  xCoordinate: [
+    "1月",
+    "2月",
+    "3月",
+    "4月",
+    "5月",
+    "6月",
+    "7月",
+    "8月",
+    "9月",
+    "10月",
+    "11月",
+    "12月",
+  ],
+  barData: [
+    33000,
+    31000,
+    36000,
+    32500,
+    34500,
+    31500,
+    33000,
+    31000,
+    36000,
+    32500,
+    34500,
+    31500,
+  ],
+  lineData: [
+    32000,
+    29000,
+    34500,
+    25800,
+    34000,
+    33000,
+    22000,
+    29000,
+    24500,
+    35800,
+    24000,
+    35000,
+  ],
+};
 
+const Trend = ({ dataSource = data }) => {
+  const options = getChartsOption(dataSource);
   useEffect(() => {
     const myChart = echarts.init(document.getElementById("rank"));
     myChart.setOption(options);
