00c71f9ea1507598687ed8343ded93dd6f51ab71
肖子立 <xiaozlb@digitalchina.com>
2021-02-25 18:15:13 +0800

    口岸大屏数据切换

--- src/pages/CbecPort/mock/index.js
+++ src/pages/CbecPort/mock/index.js
@@ -10,4 +10,66 @@ const mockPie = [
   { name: "邮局海关", value: 181 },
   { name: "东湖综保", value: 153 },
 ];
-export { SquareGridData, mockPie };
+
+const mockTable1 = [
+  {
+    name: "索尼A7R4 全画幅微单",
+    type: "进口",
+    country: "德国",
+    value: 1325357,
+    srcIndex: "shuma",
+  },
+  {
+    name: "欧乐B电动牙刷",
+    type: "进口",
+    country: "日本",
+    value: 31325357,
+    srcIndex: "priyongpin",
+  },
+  {
+    name: "福特Focus RS",
+    type: "进口",
+    country: "美国",
+    value: 23425357,
+    srcIndex: "luntai",
+  },
+  {
+    name: "海底捞火锅",
+    type: "出口",
+    country: "中本",
+    value: 23425357,
+    srcIndex: "pfood",
+  },
+];
+
+const mockTable2 = [
+  {
+    name: "索尼A7R4 全画幅微单",
+    type: "进口",
+    country: "德国",
+    value: 1325357,
+    srcIndex: "shuma",
+  },
+  {
+    name: "欧乐B电动牙刷",
+    type: "进口",
+    country: "日本",
+    value: 31325357,
+    srcIndex: "priyongpin",
+  },
+  {
+    name: "福特Focus RS",
+    type: "进口",
+    country: "美国",
+    value: 23425357,
+    srcIndex: "luntai",
+  },
+  {
+    name: "海底捞火锅",
+    type: "出口",
+    country: "中本",
+    value: 23425357,
+    srcIndex: "pfood",
+  },
+];
+export { SquareGridData, mockPie, mockTable1, mockTable2 };
